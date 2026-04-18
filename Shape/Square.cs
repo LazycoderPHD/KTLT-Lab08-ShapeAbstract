8f1baa9773bd742e610fdc6e5a2d0440b3e6dd8e
LazycoderPHD <phatdao2309@gmail.com>
2026-04-18 16:21:54 +0700

    Square done

---
+++ Shape/Square.cs
@@ -0,0 +1,36 @@
+﻿// Lớp Square kế thừa Rectangle
+public class Square : Rectangle
+{
+    public Square() : base() { }
+
+    public Square(double side) : base(side, side) { }
+
+    public Square(double side, string color, bool filled) : base(side, side, color, filled) { }
+
+    public double getSide()
+    {
+        return getWidth();
+    }
+
+    public void setSide(double side)
+    {
+        base.setWidth(side);
+        base.setLength(side);
+    }
+
+    // Ghi đè để đảm bảo hình vuông luôn có chiều dài = chiều rộng
+    public override void setWidth(double side)
+    {
+        setSide(side);
+    }
+
+    public override void setLength(double side)
+    {
+        setSide(side);
+    }
+
+    public override string toString()
+    {
+        return $"Square[{base.toString()}]";
+    }
+}
